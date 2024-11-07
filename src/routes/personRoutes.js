f573d1ab3f194cba43bb77e661ca4f1313438fbd
athena272 <guilhermera272@gmail.com>
2024-11-07 15:50:57 -0300

    Update personRoutes.js

--- src/routes/personRoutes.js
+++ src/routes/personRoutes.js
@@ -1,8 +1,9 @@
 const { Router } = require('express')
 const PersonController = require('../controller/PersonController.js')
+const personController = new PersonController()
 
 const personRouter = Router()
 
-personRouter.get('/people', PersonController.index)
+personRouter.get('/people', (req, res) => personController.index(req, res))
 
 module.exports = personRouter
