1bf54c647040802bd6ff73689f827ab3202dfce6
athena272 <guilhermera272@gmail.com>
2024-11-07 05:00:35 -0300

    Create PersonController.js

---
+++ src/controller/PersonController.js
@@ -0,0 +1,13 @@
+class PersonController {
+    // List all registers
+    static async index(req, res) {
+        try {
+
+        } catch (error) {
+
+        }
+    }
+}
+
+// Singleton instance
+module.exports = PersonController
