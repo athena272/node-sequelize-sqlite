2c09a29d833bac86577173011c09719cd3402c40
athena272 <guilhermera272@gmail.com>
2024-11-07 05:07:47 -0300

    Update PersonController.js

--- src/controller/PersonController.js
+++ src/controller/PersonController.js
@@ -1,10 +1,17 @@
+const database = require('../models')
+
 class PersonController {
     // List all registers
     static async index(req, res) {
         try {
+            const peopleList = await database.Person.findAll()
+            if (peopleList) {
+                return res.status(200).json(peopleList)
+            }
 
+            return res.status(404).json({ "message": "People not found" })
         } catch (error) {
-
+            return res.status(500).json({ message: `${error.message} - request failed` })
         }
     }
 }
