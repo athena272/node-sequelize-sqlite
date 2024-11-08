c54ad9f3f40b76a26e48022f78e4b4d0857557ff
athena272 <guilhermera272@gmail.com>
2024-11-08 06:19:12 -0300

    Update categoryRoutes.js

--- src/routes/categoryRoutes.js
+++ src/routes/categoryRoutes.js
@@ -4,10 +4,10 @@ const categoryController = new CategoryController()
 
 const categoryRouter = Router()
 
-categoryRouter.get('/people', (req, res) => categoryController.index(req, res))
-categoryRouter.get('/people/:id', (req, res) => categoryController.show(req, res))
-categoryRouter.post('/people', (req, res) => categoryController.store(req, res))
-categoryRouter.put('/people/:id', (req, res) => categoryController.update(req, res))
-categoryRouter.delete('/people/:id', (req, res) => categoryController.delete(req, res))
+categoryRouter.get('/categories', (req, res) => categoryController.index(req, res))
+categoryRouter.get('/categories/:id', (req, res) => categoryController.show(req, res))
+categoryRouter.post('/categories', (req, res) => categoryController.store(req, res))
+categoryRouter.put('/categories/:id', (req, res) => categoryController.update(req, res))
+categoryRouter.delete('/categories/:id', (req, res) => categoryController.delete(req, res))
 
 module.exports = categoryRouter
