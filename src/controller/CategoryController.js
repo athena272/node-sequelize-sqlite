4c4ae6cb8d83a5de80db05f070c99d97aecd090f
athena272 <guilhermera272@gmail.com>
2024-11-08 06:04:59 -0300

    Create CategoryController.js

---
+++ src/controller/CategoryController.js
@@ -0,0 +1,13 @@
+const Controller = require('./Controller.js')
+const CategoryServices = require('../services/CategoryServices.js')
+
+const categoryServices = new CategoryServices()
+
+class CategoryController extends Controller {
+    constructor() {
+        super(categoryServices)
+    }
+}
+
+// Singleton instance
+module.exports = CategoryController
