fd39ee1d7bb4873e02df7eecf05973afc1e0ffdc
athena272 <guilhermera272@gmail.com>
2024-11-08 06:19:17 -0300

    Update courseRoutes.js

--- src/routes/courseRoutes.js
+++ src/routes/courseRoutes.js
@@ -4,10 +4,10 @@ const courseController = new CourseController()
 
 const courseRouter = Router()
 
-courseRouter.get('/people', (req, res) => courseController.index(req, res))
-courseRouter.get('/people/:id', (req, res) => courseController.show(req, res))
-courseRouter.post('/people', (req, res) => courseController.store(req, res))
-courseRouter.put('/people/:id', (req, res) => courseController.update(req, res))
-courseRouter.delete('/people/:id', (req, res) => courseController.delete(req, res))
+courseRouter.get('/courses', (req, res) => courseController.index(req, res))
+courseRouter.get('/courses/:id', (req, res) => courseController.show(req, res))
+courseRouter.post('/courses', (req, res) => courseController.store(req, res))
+courseRouter.put('/courses/:id', (req, res) => courseController.update(req, res))
+courseRouter.delete('/courses/:id', (req, res) => courseController.delete(req, res))
 
 module.exports = courseRouter
