549b5ffdede2166a9d69459ed6df8974e7488ae9
athena272 <guilhermera272@gmail.com>
2024-11-08 01:03:03 -0300

    Update Services.js

--- src/services/Services.js
+++ src/services/Services.js
@@ -9,6 +9,29 @@ class Services {
     async index() {
         return await database[this.model].findAll()
     }
+
+    async show(id) {
+        return await database[this.model].findByPk(id)
+    }
+
+    async store(registerData) {
+        return await database[this.model].create(registerData)
+    }
+
+    async update({ newData, id }) {
+        const listRegisters = await database[this.model].update(newData, {
+            where: { id: id },
+        })
+        if (listRegisters[0] === 0) {
+            return false
+        }
+
+        return true
+    }
+
+    async delete(id) {
+        return await database[this.model].destroy({ where: { id: id } })
+    }
 }
 
 module.exports = Services
