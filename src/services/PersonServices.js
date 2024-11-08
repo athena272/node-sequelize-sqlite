7f96dd5888e2d5c2cc40f4f27005e0147f0ca9ad
athena272 <guilhermera272@gmail.com>
2024-11-08 07:32:11 -0300

    Update PersonServices.js

--- src/services/PersonServices.js
+++ src/services/PersonServices.js
@@ -4,6 +4,12 @@ class PersonServices extends Services {
     constructor() {
         super('Person')
     }
+
+    async getRegistrationsByStudent(id) {
+        const student = await super.show(id)
+        const listRegistrations = await await student.getEnrolledClasses()
+        return listRegistrations
+    }
 }
 
 module.exports = PersonServices
