b03856020b4a360a33b1681d57403155160d7277
athena272 <guilhermera272@gmail.com>
2024-11-08 06:09:59 -0300

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const personRouter = require('./personRoutes.js')
+const categoryRouter = require('./categoryRoutes.js')
+const courseRouter = require('./courseRoutes.js')
 
 const routes = (app) => {
     app.route("/").get((req, res) => res.status(200).json({ "message": "Server on 🔥" }))
 
-    app.use(express.json(), personRouter)
+    app.use(express.json(), personRouter, categoryRouter, courseRouter)
 }
 
 module.exports = routes
