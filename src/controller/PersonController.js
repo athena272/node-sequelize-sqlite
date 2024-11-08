18680241ae3f97a5a77d591f940209019f15dffb
athena272 <guilhermera272@gmail.com>
2024-11-08 07:32:21 -0300

    Update PersonController.js

--- src/controller/PersonController.js
+++ src/controller/PersonController.js
@@ -7,6 +7,20 @@ class PersonController extends Controller {
     constructor() {
         super(personServices)
     }
+
+    async getRegistrations(req, res) {
+        try {
+            const { studentId } = req.params
+            const listRegistrations = await personServices.getRegistrationsByStudent(Number(studentId))
+            if (listRegistrations) {
+                return res.status(200).json(listRegistrations)
+            }
+
+            return res.status(404).json({ "message": `Register not found` })
+        } catch (error) {
+            return res.status(500).json({ message: `Error 500 - request failed` })
+        }
+    }
 }
 
 // Singleton instance
