a8acb912caedc9f29d074cc1e06b4cca7d07ca51
athena272 <guilhermera272@gmail.com>
2024-11-08 07:32:02 -0300

    Update personRoutes.js

--- src/routes/personRoutes.js
+++ src/routes/personRoutes.js
@@ -12,6 +12,7 @@ router.post('/people', (req, res) => personController.store(req, res))
 router.put('/people/:id', (req, res) => personController.update(req, res))
 router.delete('/people/:id', (req, res) => personController.delete(req, res))
 //Registration routes
+router.get('/people/:studentId/registration', (req, res) => personController.getRegistrations(req, res))
 router.post('/people/:studentId/registration', (req, res) => registrationController.store(req, res))
 
 module.exports = router
