a53b9b82b6e7a0a6aace64c699dc0f54ea690f44
athena272 <guilhermera272@gmail.com>
2024-11-08 02:58:37 -0300

    Update person.js

--- src/models/person.js
+++ src/models/person.js
@@ -10,7 +10,12 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Person.hasMany(models.Course, {
+        foreignKey: 'teacher_id'
+      })
+      Person.hasMany(models.Registration, {
+        foreignKey: 'student_id'
+      })
     }
   }
   Person.init({
