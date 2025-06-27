e47dd09bee6bc09d9f4aa6790f62a70b8cf3b525
rosario <rosario@bne.com.br>
2025-06-27 20:56:06 -0300

    Create validateCPF.js

---
+++ src/utils/validateCPF.js
@@ -0,0 +1,29 @@
+function validateCPF(cpf) {
+    if (typeof cpf !== 'string') return false;
+
+    cpf = cpf.replace(/[^\d]+/g, '');
+
+    if (cpf.length !== 11 || /^(\d)\1+$/.test(cpf)) return false;
+
+    let sum = 0;
+    for (let i = 0; i < 9; i++) {
+        sum += parseInt(cpf.charAt(i)) * (10 - i);
+    }
+
+    let firstCheckDigit = 11 - (sum % 11);
+    if (firstCheckDigit >= 10) firstCheckDigit = 0;
+
+    if (firstCheckDigit !== parseInt(cpf.charAt(9))) return false;
+
+    sum = 0;
+    for (let i = 0; i < 10; i++) {
+        sum += parseInt(cpf.charAt(i)) * (11 - i);
+    }
+
+    let secondCheckDigit = 11 - (sum % 11);
+    if (secondCheckDigit >= 10) secondCheckDigit = 0;
+
+    return secondCheckDigit === parseInt(cpf.charAt(10));
+}
+
+module.exports = validateCPF;
