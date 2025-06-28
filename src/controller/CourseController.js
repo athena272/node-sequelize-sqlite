6ae67ceeeaa30ffb1a3071fbfd2e28baad043b21
rosario <rosario@bne.com.br>
2025-06-28 13:20:37 -0300

    Update CourseController.js

--- src/controller/CourseController.js
+++ src/controller/CourseController.js
@@ -1,11 +1,26 @@
+const { Op } = require('sequelize')
+
 const Controller = require('./Controller.js')
 const CourseServices = require('../services/CourseServices.js')
+const { sequelize } = require('../database/models/index.js')
 
 const courseServices = new CourseServices()
 
 class CourseController extends Controller {
     constructor() {
         super(courseServices)
+    }
+
+    async getCourses(req, res) {
+        const { final_date, start_date } = req.query
+        const where = {}
+
+        //if params, create a prop
+        start_date || final_date ? where.start_date = {} : null
+        //if start_date, add gte
+        start_date ? where.start_date[Op.gte] = start_date : null
+        //if final_date
+        final_date ? where.start_date[Op.lte] = final_date : null
     }
 }
 
