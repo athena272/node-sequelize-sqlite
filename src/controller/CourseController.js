3a5f00aa51d589e0cc092dfcc91eb02a704a6930
rosario <rosario@bne.com.br>
2025-06-28 13:54:13 -0300

    Update CourseController.js

--- src/controller/CourseController.js
+++ src/controller/CourseController.js
@@ -21,7 +21,20 @@ class CourseController extends Controller {
         start_date ? where.start_date[Op.gte] = start_date : null
         //if final_date
         final_date ? where.start_date[Op.lte] = final_date : null
+
+        try {
+            const coursesList = await courseServices.index(where)
+            if (coursesList) {
+                return res.status(200).json(coursesList)
+            }
+
+            return res.status(404).json({ "message": `${this.serviceEntity} not found` })
+        } catch (error) {
+            return res.status(500).json({ message: `${error.message} - request failed` })
+        }
     }
+
+
 }
 
 // Singleton instance
