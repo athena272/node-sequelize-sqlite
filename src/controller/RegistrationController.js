39ddf9c2ec58d2f3fe7550c83d4a36216fb88cfe
rosario <rosario@bne.com.br>
2025-06-28 18:14:01 -0300

    Update RegistrationController.js

--- src/controller/RegistrationController.js
+++ src/controller/RegistrationController.js
@@ -24,6 +24,24 @@ class RegistrationController extends Controller {
             return res.status(500).json({ message: `${error.message} - request failed` });
         }
     }
+
+    async getRegistrationsByStudent(req, res) {
+        try {
+            const { ...params } = req.params
+            // console.log("🚀 ~ Controller ~ showWhere ~ params:", params)
+            const where = convertStringIdsToNumbers(params)
+            // console.log("🚀 ~ Controller ~ showWhere ~ where:", where)
+
+            const registerFound = await this.serviceEntity.showWhere(where)
+            if (registerFound) {
+                return res.status(200).json(registerFound)
+            }
+
+            return res.status(404).json({ "message": `${this.serviceEntity} not found` })
+        } catch (error) {
+            return res.status(500).json({ message: `${error.message} - request failed` })
+        }
+    }
 }
 
 // Singleton instance
